7b45c6a57dedf705b7c835608cbfc59b31e99059
Aleksandr Omelucik <bladewizard@mail.ru>
2025-04-01 09:49:04 +0300

    up

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let postTitle = "";
+let post = "";
 
 const titleNote = document.getElementById("post-title");
 const inputNote = document.getElementById("post-input");
@@ -6,7 +6,20 @@ const buttonNote = document.getElementById("post-button");
 const postNode = document.getElementById("post");
 
 buttonNote.addEventListener("click", function() {
-    const postTitle = inputNote.value;
-    postNode.innerText = postTitle;
-    console.log(postTitle);
+    const postFromUser = getPostFromUser();
+    setPost(postFromUser);
+    renderPost();
 });
+
+function getPostFromUser() {
+    const post = inputNote.value;
+    
+    return post;
+}
+
+function setPost(newPost) {
+    post = newPost;
+}
+function renderPost() {
+    postNode.innerText = post;
+}
