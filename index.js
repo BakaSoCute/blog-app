aaa3b79f7ad4ded492444e68288e9f1c55921371
Aleksandr Omelucik <bladewizard@mail.ru>
2025-04-02 08:48:43 +0300

    update

--- index.js
+++ index.js
@@ -1,25 +1,61 @@
-let post = "";
+const posts = [];
 
-const titleNote = document.getElementById("post-title");
+const postTextInputNode = document.getElementById("post_input_text")
 const inputNote = document.getElementById("post-input");
 const buttonNote = document.getElementById("post-button");
 const postNode = document.getElementById("post");
+const ErorText = document.getElementById("error")
 
 buttonNote.addEventListener("click", function() {
     const postFromUser = getPostFromUser();
-    setPost(postFromUser);
-    renderPost();
+    let eror = ErorText;
+
+    if (postFromUser.title === "") {
+        eror.innerHTML = "введите название"
+        return
+    }
+    if (postFromUser.text === "") {
+        eror.innerHTML = "введите текст"
+        return
+    }
+    eror.innerHTML = "";
+    addPost(postFromUser);
+    renderPosts();
 });
 
 function getPostFromUser() {
-    const post = inputNote.value;
+    const title = inputNote.value;
+    const text = postTextInputNode.value;
+    const date = new Date().toLocaleString();
     
-    return post;
+    return {
+        title: title,
+        text: text,
+        date: date
+    };
 }
 
-function setPost(newPost) {
-    post = newPost;
+function addPost({title, text, date}) {
+    posts.push({
+        title,
+        text,
+        date
+    });
 }
-function renderPost() {
-    postNode.innerText = post;
+function getPosts() {
+    return posts;
+}
+function renderPosts() {
+    const posts = getPosts();
+    let postsHTML = "";
+    posts.forEach(post => {
+        postsHTML += `
+            <div class="post">
+            <p class="post__title">${post.title}</p>
+            <p class="post__text">${post.text}</p>
+            <p class="post__text">${post.date}</p>
+            </div>
+        `;
+    });
+    postNode.innerHTML = postsHTML;
 }
